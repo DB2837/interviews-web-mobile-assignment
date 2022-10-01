ca52d13464d4e72097afc3a64e65531c77f02778
DB2837 <davidebellucci975@gmail.com>
2022-10-01 16:53:53 +0200

    add error box component for a failed fetch feedback

---
+++ frontend/src/components/ErrorBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ErrorBox = () => {
+  return (
+    <>
+      <div>
+        <h3>Something went wrong, failed to fetch posts.</h3>
+      </div>
+    </>
+  );
+};
+
+export default ErrorBox;
