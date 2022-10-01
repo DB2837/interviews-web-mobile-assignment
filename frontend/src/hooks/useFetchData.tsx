2c03f6c5499d0e6a7e92419d5ae656a6a706e2de
DB2837 <davidebellucci975@gmail.com>
2022-10-01 16:51:50 +0200

    add custom fetch hook to fetch data passing an URL. The hook gives information on the status of the fetching(is loading?)  and if there have been errors on the process whit boolean states.

---
+++ frontend/src/hooks/useFetchData.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+const useFetchData = (url: string) => {
+  const [data, setData] = useState<React.SetStateAction<any>>(null);
+  const [error, setError] = useState<React.SetStateAction<boolean>>(false);
+  const [loading, setLoading] = useState<React.SetStateAction<boolean>>(true);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    (async () => {
+      try {
+        const data = await fetch(url, { signal: signal }).then((response) =>
+          response?.json()
+        );
+
+        setData(data);
+        setError(false);
+      } catch (err) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    })();
+
+    return () => controller.abort();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetchData;
