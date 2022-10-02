554d52f0053e01071e7a9dd5c0a8a8b3dfc1431e
DB2837 <davidebellucci975@gmail.com>
2022-10-02 09:35:42 +0200

    code refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import ErrorBox from './components/ErrorBox';
 import Loader from './components/Loader';
-import PostCard, { TPost } from './components/PostCard';
+import Posts from './components/posts/Posts';
 import useFetchData from './hooks/useFetchData';
 
-const URL = 'https://jsonplaceholder.typicode.com/posts?_page=0&_limit=6';
+const URL = `https://jsonplaceholder.typicode.com/posts?_page=0&_limit=6`;
 
 function App() {
-  const { data: posts, error, loading } = useFetchData(URL);
+  const { data: posts, setData: setPosts, error, loading } = useFetchData(URL);
 
   return (
     <PageContainer>
@@ -17,24 +17,7 @@ function App() {
       ) : error ? (
         <ErrorBox />
       ) : (
-        <>
-          {' '}
-          <>
-            <PostsContainer>
-              {posts?.length > 0 &&
-                posts.map((post: TPost) => {
-                  return (
-                    <PostCard
-                      body={post.body}
-                      id={post.id}
-                      title={post.title}
-                      userId={post.userId}
-                    />
-                  );
-                })}
-            </PostsContainer>
-          </>
-        </>
+        <Posts posts={posts} setPosts={setPosts} />
       )}
     </PageContainer>
   );
@@ -47,12 +30,4 @@ const PageContainer = styled.div`
   display: flex;
   justify-content: center;
   padding: 3rem;
-`;
-
-const PostsContainer = styled.main`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  max-width: 1100px;
-  min-width: 320px;
 `;
